817546107be6bd422e68d0ea2e9d71eaf67484ce
Jasper den Hertog <jasper@plainspace.com>
2023-10-02 15:59:29 +0200

    restrict mod visibility

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 pub mod ast;
 mod attr_change_set;
 pub mod compile;
-mod eval;
-pub mod parse_string;
-pub mod symbols;
-pub mod traits;
+pub mod eval;
+mod parse_string;
+mod symbols;
+mod traits;
 pub mod types;
 pub mod vm;
 pub mod blocks;
